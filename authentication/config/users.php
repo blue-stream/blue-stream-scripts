66742f01e0b7ece20d63f47c21cd9d688b75e121
Ron Borysovski <roni537@gmail.com>
2019-01-24 16:32:35 +0200

    chore: Add user data

---
+++ authentication/config/users.php
@@ -0,0 +1,36 @@
+<?php
+
+$config = array(
+
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'borysovski:ron' => array(
+            'id' => array('roni537@gmail'),
+            'givenName' => 'Ron',
+            'surName' => 'Borysovski',
+            'mail' => 'roni537@gmail.com'
+        ),
+        'assa:yuval' => array(
+            'id' => array('assa.yuval107'),
+            'givenName' => 'Yuval',
+            'surName' => 'Assa',
+            'mail' => 'assa.yuval107.com'
+        ),
+        'vagman:almog' => array(
+            'id' => array('almogvc@gmail'),
+            'givenName' => 'Almog',
+            'surName' => 'Vagman',
+            'mail' => 'almogvc@gmail.com'
+        ),
+        'mornel:saar' => array(
+            'id' => array('saar@gmail'),
+            'givenName' => 'Saar',
+            'surName' => 'Mornel',
+            'mail' => 'saar@gmail.com'
+        )
+    )
+);
