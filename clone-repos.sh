ed5b3a0327d5f699c1f7b41c9de96781545bbcb2
SirSaar <saar643@gmail.com>
2019-02-24 14:48:24 +0200

    feat: auto clone

---
+++ clone-repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ..
+
+git clone https://github.com/blue-stream/blue-stream-video
+git clone https://github.com/blue-stream/blue-stream-transcoder
+git clone https://github.com/blue-stream/blue-stream-uploader
+git clone https://github.com/blue-stream/blue-stream-comments
+git clone https://github.com/blue-stream/blue-stream-reactions
+git clone https://github.com/blue-stream/blue-stream-channel
+git clone https://github.com/blue-stream/blue-stream-users
+git clone https://github.com/blue-stream/blue-stream-streamer
+git clone https://github.com/blue-stream/blue-stream-classification-mocks
+git clone https://github.com/blue-stream/blue-stream-video-compositor
+git clone https://github.com/blue-stream/blue-stream-comment-compositor
+git clone https://github.com/blue-stream/blue-stream-reaction-compositor
+git clone https://github.com/blue-stream/blue-stream-authenticator
+git clone https://github.com/blue-stream/blue-stream-video-compositor
+git clone https://github.com/blue-stream/blue-stream-client
