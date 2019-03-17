e44e12892590cdc10788416401d48397a6aa6da5
SirSaar <saar643@gmail.com>
2019-03-17 14:54:12 +0200

    feat: add helper docker images to script

--- build-images.sh
+++ build-images.sh
@@ -11,3 +11,8 @@ for D in *; do
         docker save --output docker-images/"$D"-v1.0.tar blue-stream/"$D":1.0
     fi
 done
+
+docker save --output docker-images/rabbitmq.tar rabbitmq:management
+docker save --output docker-images/minio.tar minio/minio:latest
+docker save --output docker-images/test-saml-idp kristophjunge/test-saml-idp:latest
+docker save --output docker-images/mongo.tar mongo:latest
