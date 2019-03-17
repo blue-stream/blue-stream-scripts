0c0ed06592f67ef572443aaeef8cae022ece656c
SirSaar <saar643@gmail.com>
2019-03-17 17:29:04 +0200

    fix: add tar

--- build-images.sh
+++ build-images.sh
@@ -14,5 +14,5 @@ done
 
 docker save --output docker-images/rabbitmq.tar rabbitmq:management
 docker save --output docker-images/minio.tar minio/minio:latest
-docker save --output docker-images/test-saml-idp kristophjunge/test-saml-idp:latest
+docker save --output docker-images/test-saml-idp.tar kristophjunge/test-saml-idp:latest
 docker save --output docker-images/mongo.tar mongo:latest
